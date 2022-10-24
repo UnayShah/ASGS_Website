51e0ad2d4cbe1b5f66ca102d3983fb9c5cec2a2f
Khushi-Purohit <unayshah@gmail.com>
2022-10-24 15:20:23 -0700

    Removed border radius changing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,21 +2,19 @@ import { stringify } from "querystring";
 import React, { useEffect, useState } from "react";
 const Hero = () => {
         const [imageMargin, setImageMargin] = useState('0px')
-        const [imageBorderRadius, setImageBorderRadius] = useState('0px')
 
         useEffect(() => {
                 const handleMargin = () => {
                         setImageMargin((window.scrollY / 20) + 'px');
-                        setImageBorderRadius((window.scrollY / 10) + 'px');
                 }
                 window.addEventListener('scroll', handleMargin);
         })
 
         return (
                 <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center main-image"
-                        style={{ marginLeft: `${imageMargin}`, marginRight: `${imageMargin}`, borderBottomLeftRadius: `${imageBorderRadius}`, borderBottomRightRadius: `${imageBorderRadius}`, }}>
+                        style={{ marginLeft: `${imageMargin}`, marginRight: `${imageMargin}`, }}>
                         <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[2]"
-                                style={{ marginLeft: `${imageMargin}`, marginRight: `${imageMargin}`, borderBottomLeftRadius: `${imageBorderRadius}`, borderBottomRightRadius: `${imageBorderRadius}`, }} />
+                                style={{ marginLeft: `${imageMargin}`, marginRight: `${imageMargin}`, }} />
                         <div className="text-white z-[2] ml-10 mt-10">
                                 <h1 className="text-7xl bold">Lorem Ipsum</h1>
                         </div>
